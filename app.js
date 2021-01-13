d40d6ebcf625250e444710c83ccc27c57f48bee3
Jhoandris Baron <andybaronp@gmail.com>
2021-01-13 17:54:41 -0500

    add check

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const form = document.getElementById('form');
 const btn = document.getElementById('btn')
+const check = document.getElementById('check')
 btn.disabled = true
 
+// console.log(check.value);
+
 const inputs = [code, number]
-
-
 inputs.forEach((elemen) => {
 
 
@@ -19,12 +20,22 @@ inputs.forEach((elemen) => {
 })
 
 
+check.addEventListener('click', () => {
+
+    if (check.value) {
+        message.classList.toggle('off')
+    }
+
+
+})
+
 
 form.addEventListener('submit', e => {
     e.preventDefault()
     const code = parseInt(document.getElementById('code').value)
     const number = parseInt(document.getElementById('number').value)
     const message = document.getElementById('message').value
+
 
     const url = `https://wa.me/${code}${number}?text=${message}`
     window.open(url)
